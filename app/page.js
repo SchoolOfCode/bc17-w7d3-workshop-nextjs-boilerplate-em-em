b7380c21748fdb934c24fa36da634008f3ab06df
Crom <daffodil.luck9@gmail.com>
2024-08-14 15:59:48 +0100

    add founder page and tested a link

--- app/page.js
+++ app/page.js
@@ -2,6 +2,22 @@ import Image from "next/image";
 import "./page.css";
 import Main from "./components/Main/Main.jsx"
 
+import Link from 'next/link';
+
 export default function Home() {
-  return <Main />;
+
+  return (
+    <>
+      <Main />
+      <Page />
+      
+    </>
+
+  );
+}
+
+
+ 
+function Page() {
+  return <Link href="/dashboard">Dashboard</Link>
 }
