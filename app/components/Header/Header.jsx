42b000357824481b1e32ac0b45d1e6661f0ae33c
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-08-14 16:55:39 +0100

    Set up event listener that displays a div upon clicking the burger menu image.

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,12 +1,26 @@
+'use client';
+
 import React from "react";
+import { useState } from "react";
 
 import styles from "./Header.module.css";
 
+
+
 const Header = function () {
+
+    const [menuStatus, setMenuStatus] = useState(false);
+
+    function handleClick (){
+        setMenuStatus(!menuStatus);
+        console.log(menuStatus);
+    }
+
     return (
         <header className={styles.siteHeader}>
             <h1 className={styles.siteTitle}>🔥Fireplace Palace</h1>
-            <button><img className={styles.burgerMenu} src="images/menu-open-button.png" alt="burger menu button" /></button>
+            <img onClick = {handleClick} className={styles.burgerMenu} src="images/menu-open-button.png" alt="burger menu button" />
+            {menuStatus && <div className = "menu">hello guys</div>}
         </header>
     );
 }
