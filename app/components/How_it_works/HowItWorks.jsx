83be9659e6b0c5264ad41936e88b3eaa80c44bf0
Crom <daffodil.luck9@gmail.com>
2024-08-14 14:27:43 +0100

    added images to the project

--- app/components/How_it_works/HowItWorks.jsx
+++ app/components/How_it_works/HowItWorks.jsx
@@ -3,28 +3,28 @@ import styles from "./HowItWorks.module.css";
 import Card from "../Card/Card.jsx";
 
 const HowItWorks = function() {
-    // const cards = [{
-    //     image: "images/how-it-works-1.png",
-    //     image_alt: "couches", 
-    //     heading: "Give us a call ...",   
-    //     text: "Call us and book in a 'Design Consultation' for a date and time to suit you.", 
-    // }, {
-    //     image: "images/how-it-works-2.png",
-    //     image_alt: "clean desk", 
-    //     heading: "We come to you ...",   
-    //     text: "We come to your home to do an assessment of the space and to your style preference.", 
-    // }, {
-    //     image: "images/how-it-works-3.png",
-    //     image_alt: "couch", 
-    //     heading: "We recommend ...",   
-    //     text: "We send you a bespoke set of fireplace recommendations.", 
-    // }]
-    const card = {
+    const cards = [{
         image: "images/how-it-works-1.png",
         image_alt: "couches", 
         heading: "Give us a call ...",   
         text: "Call us and book in a 'Design Consultation' for a date and time to suit you.", 
-    }
+    }, {
+        image: "images/how-it-works-2.png",
+        image_alt: "clean desk", 
+        heading: "We come to you ...",   
+        text: "We come to your home to do an assessment of the space and to your style preference.", 
+    }, {
+        image: "images/how-it-works-3.png",
+        image_alt: "couch", 
+        heading: "We recommend ...",   
+        text: "We send you a bespoke set of fireplace recommendations.", 
+    }]
+    // const card = {
+    //     image: "images/how-it-works-1.png",
+    //     image_alt: "couches", 
+    //     heading: "Give us a call ...",   
+    //     text: "Call us and book in a 'Design Consultation' for a date and time to suit you.", 
+    // }
     return(
         <>
             <section className={styles.how_it_works}>
