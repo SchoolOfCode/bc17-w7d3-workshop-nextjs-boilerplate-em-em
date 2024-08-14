ff314d3d7f2e9bd4d41128f386060047c2015272
Ryan <ryanshaunsmith@gmail.com>
2024-08-14 16:38:21 +0100

    added founder images and burgerMenu button

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = function () {
     return (
         <header className={styles.siteHeader}>
             <h1 className={styles.siteTitle}>🔥Fireplace Palace</h1>
+            <button><img className={styles.burgerMenu} src="images/menu-open-button.png" alt="burger menu button" /></button>
         </header>
     );
 }
