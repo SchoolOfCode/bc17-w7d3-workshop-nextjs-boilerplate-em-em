be4c1ff5553feb2f2bad5fb664041efb58de6b23
Crom <daffodil.luck9@gmail.com>
2024-08-15 10:37:02 +0100

    added links to Home and Founders Page

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useState } from "react";
 
 import styles from "./Header.module.css";
-
+import Link from "next/link";
 
 
 const Header = function () {
@@ -17,12 +17,22 @@ const Header = function () {
     }
 
     return (
-        <header className={styles.siteHeader}>
-            <h1 className={styles.siteTitle}>🔥Fireplace Palace</h1>
-            <img onClick = {handleClick} className={styles.burgerMenu} src="images/menu-open-button.png" alt="burger menu button" />
-            {menuStatus && <div className = "menu">hello guys</div>}
-        </header>
-    );
+			<header className={styles.siteHeader}>
+				<h1 className={styles.siteTitle}>🔥Fireplace Palace</h1>
+				<img
+					onClick={handleClick}
+					className={styles.burgerMenu}
+					src="images/menu-open-button.png"
+					alt="burger menu button"
+				/>
+				{menuStatus && (
+					<div className="menu">
+						<Link href="/">Home</Link>
+						<Link href="/founders">Founders</Link>
+					</div>
+				)}
+			</header>
+		);
 }
 
 export default Header;
--- app/page.js
+++ app/page.js
@@ -2,22 +2,16 @@ import Image from "next/image";
 import "./page.css";
 import Main from "./components/Main/Main.jsx"
 
-import Link from 'next/link';
+
 
 export default function Home() {
 
   return (
     <>
       <Main />
-      <Page />
+     
       
     </>
 
   );
-}
-
-
- 
-function Page() {
-  return <Link href="/dashboard">Dashboard</Link>
 }
