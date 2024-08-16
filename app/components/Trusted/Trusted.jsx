686d98d2d22e601025a4a347f3eaba4e350df178
Crom <daffodil.luck9@gmail.com>
2024-08-16 10:35:16 +0100

    start button styling

--- app/components/Trusted/Trusted.jsx
+++ app/components/Trusted/Trusted.jsx
@@ -8,7 +8,7 @@ export default function Trusted () {
             <h2 className = {styles.trustedTitle}>Trusted.</h2>
             <p className={styles.trustedText}>We've got thousands of happy customers all over the UK. Choose your country to see the latest review</p>
             <div className={styles.review}>
-                <ul>
+                <ul className={styles.trustedList}>
                     <li><Button /></li>
                     <li><Button /></li>
                     <li><Button /></li>
