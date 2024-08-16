c324d7c5bde7d692615896ed1ae053658a295fd1
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-08-16 12:28:14 +0100

    commented out Trusted.jsx file

--- app/components/Trusted/Trusted.jsx
+++ app/components/Trusted/Trusted.jsx
@@ -8,9 +8,10 @@ export default function Trusted () {
 
     // Set some state variables and their corresponding setter functions
 
-    const [selectedCountry, setSelectedCountry] = useState(null); // selectedCountry represents
-    const [selectedReview, setSelectedReview] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState(null); // selectedCountry represents the country that the review is from
+    const [selectedReview, setSelectedReview] = useState([]); // selectedReview contains the fetched review
  
+    // useEffect which will fetch a review from the corresponding selectedCountry, only if selectedCountry's state changes
     useEffect ( ()=> {
         fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
         .then(response => response.json())
@@ -24,13 +25,18 @@ export default function Trusted () {
             <p className={styles.trustedText}>We've got thousands of happy customers all over the UK. Choose your country to see the latest review</p>
             <div className={styles.review}>
                 <ul className={styles.trustedList}>
+                    {/* each time the buttons get clicked, the setSelectedCountry setter function changes the
+                    state of selectedCountry to the specified country. This consequently triggers the useEffect
+                    to fetch the selectedReview for that corresponding country */}
                     <li><button onClick={()=> setSelectedCountry('england')} className={styles.button}>England</button></li>
                     <li><button onClick={()=> setSelectedCountry('wales')} className={styles.button}>Wales</button></li>
                     <li><button onClick={()=> setSelectedCountry('scotland')} className={styles.button}>Scotland</button></li>
                 </ul>
                 <div>
+                    {/* this returns the actual review, turned in to string content. */}
                     <p>{JSON.stringify(selectedReview.text)}</p>
                     <br/ >
+                    {/* this returns the review's author, turned in to string content. */}
                     <p>{JSON.stringify(selectedReview.author)}</p>
                 </div>
             </div>
