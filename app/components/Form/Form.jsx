64b2129ef2a80755d2ef66b1d97123a9c082250e
Ryan <ryanshaunsmith@gmail.com>
2024-08-20 11:16:05 +0100

    added form inputs

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -6,10 +6,34 @@ export default function Form() {
         <form method='post'>
             <fieldset className = {styles.personalInfo}>
                 <legend>Personal Information: </legend>
+                <label htmlFor="">
+                    Full Name: * <br />
+                    <input type="text" name='fullName'/>
+                </label>
+                <label htmlFor="">
+                    Postcode: * <br />
+                    <input type="text" name='postcode'/>
+                </label>
+                <label>
+                    House/Flat Number and Street Name: * <br />
+                    <input type="text" name='street_address'/>
+                </label>
+                <label htmlFor="">
+                    City: * <br />
+                    <input type="text" name='city'/>
+                </label>
             </fieldset>
 
             <fieldset className = {styles.contactInfo}>
                 <legend>Contact Information: </legend>
+                <label htmlFor="">
+                    Phone Number: * <br />
+                    <input type="text" name='phone_num'/>
+                </label>
+                <label htmlFor="">
+                    Email Address: * <br />
+                    <input type="text" name='email'/>
+                </label>
             </fieldset>
         </form>
     );
