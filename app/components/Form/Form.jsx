8bdba95e7a165248a0f152dfdc4fb2f851c8dc53
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-08-20 12:21:23 +0100

    Created state to store full name input, and defined a function that handles and sets the full name input.

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -1,14 +1,28 @@
+'use client';
 import styles from './Form.module.css';
 import React from 'react';
+import { useState } from 'react';
 
 export default function Form() {
+
+    // Create state that stores the content inside fullname input box
+    const [fullName, setFullName] = useState('');
+
+    // Define a function that 
+    const handleFullName = function (event) {
+        // Change state of fullName to the inputted value
+        setFullName(event.target.value);
+    };
+
+    console.log('full name: ', fullName);
+
     return (
         <form method='post'>
             <fieldset className = {styles.personalInfo}>
                 <legend>Personal Information: </legend>
                 <label htmlFor="">
                     Full Name: * <br />
-                    <input type="text" name='fullName'/>
+                    <input type="text" name='fullName' onChange = {(e)=>handleFullName(e)} value = {fullName}/>
                 </label>
                 <label htmlFor="">
                     Postcode: * <br />
