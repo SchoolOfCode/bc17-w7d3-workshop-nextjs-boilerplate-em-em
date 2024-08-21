76f2f071e053c207deb89b83bbfcab0ffa2f9c1f
Crom <daffodil.luck9@gmail.com>
2024-08-21 14:26:44 +0100

    started refactoring to use the reducer hook

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -1,85 +1,93 @@
-'use client';
-import styles from './Form.module.css';
-import React from 'react';
-import { useState } from 'react';
-
-
+"use client";
+import styles from "./Form.module.css";
+import React from "react";
+import { useState, useReducer } from "react";
 
 // Define initial state for one value
 const initialState = {
-    data : {
-        fullName : ""
-    },
-    error : false
-}
-
+	data: {
+		fullName: "",
+	},
+	error: false,
+};
 
 // Write reducer function
 const reducer = (state, action) => {
-    switch (action.type){
-        case 'CHANGE_FIELD': 
-        return {
-            data : {
-                ...state.data, [action.payload.fieldName] : action.payload.fieldValue
-            }
-        } 
-        default:
-        return state;
-    }
-}
+	switch (action.type) {
+		case "CHANGE_FIELD":
+			return {
+				data: {
+					...state.data,
+					[action.payload.fieldName]: action.payload.fieldValue,
+				},
+			};
+		default:
+			return state;
+	}
+};
 
 export default function Form() {
+	// Create state that stores the content inside fullname input box
+	const [fullName, setFullName] = useState("");
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-    // Create state that stores the content inside fullname input box
-    const [fullName, setFullName] = useState('');
+	// Define a function that
+	const handleChange = function (event) {
+		// Change state of fullName to the inputted value
+		if (event.target.name === "fullName") {
+			dispatch({
+				type: "CHANGE_FIELD",
+				payload: {
+					fieldName: event.target.name,
+					fieldValue: event.target.value,
+				},
+			});
+		}
+	};
 
-    // Define a function that 
-    const handleChange = function (event) {
-        // Change state of fullName to the inputted value
-        if(event.target.name === "fullName" ) {
-            setFullName(event.target.value);
-        }
-    };
+	// Create state to show error status (false means no error)
+	const [isError, setIsError] = useState(false);
 
-    // Create state to show error status (false means no error)
-    const [isError, setIsError] = useState(false);
+	// Create handler function for submitting form.
+	const handleSubmit = function (event) {
+		event.preventDefault();
+		// should check for empty fields
+		// if any field is empty,
+		if (fullName === "") {
+			// should display error and prompt to fill form
+			setIsError(!isError);
+			console.log("line 60 form.jsx error - all fields required");
+			// else, console log filled fields
+		} else {
+			console.log("full name: ", fullName);
+			setIsError(false);
+		}
+	};
 
-    // Create handler function for submitting form.
-    const handleSubmit = function (event) {
-        event.preventDefault();
-        // should check for empty fields
-        // if any field is empty,
-        if (fullName === "") {     
-        // should display error and prompt to fill form
-        setIsError(!isError);
-    console.log('line 60 form.jsx error - all fields required')
-        // else, console log filled fields
-        } else { 
-            console.log('full name: ', fullName);
-            setIsError(false)
-        };
-    };
+	return (
+		<form method="post" onSubmit={handleSubmit}>
+			<fieldset className={styles.personalInfo}>
+				<legend>Personal Information: </legend>
+				<label>
+					Full Name: * <br />
+					<input
+						type="text"
+						name="fullName"
+						onChange={(e) => handleChange(e)}
+						value={fullName}
+					/>
+				</label>
+			</fieldset>
 
-    return (
-			<form method="post" onSubmit={handleSubmit}>
-				<fieldset className={styles.personalInfo}>
-					<legend>Personal Information: </legend>
-					<label>
-						Full Name: * <br />
-						<input
-							type="text"
-							name="fullName"
-							onChange={(e) => handleChange(e)}
-							value={fullName}
-						/>
-					</label>
-				</fieldset>
-
-				<fieldset className={styles.contactInfo}>
-					<legend>Contact Information: </legend>
-				</fieldset>
-                {isError && <p style={{color:"red"}}>Error all fields are required - some missing</p>}
-				<button type="submit">Request Design Consultation</button>
-			</form>
-		);
+			<fieldset className={styles.contactInfo}>
+				<legend>Contact Information: </legend>
+			</fieldset>
+			{isError && (
+				<p style={{ color: "red" }}>
+					Error all fields are required - some missing
+				</p>
+			)}
+			<button type="submit">Request Design Consultation</button>
+		</form>
+	);
 }
