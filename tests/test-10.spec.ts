36562c9ab2c47ad4213433148ade6008cdd73005
Crom <daffodil.luck9@gmail.com>
2024-08-23 10:08:38 +0100

    start writing test

--- tests/test-10.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('heading', { name: '🔥Fireplace Palace' }).click();
-  await page.getByRole('img', { name: 'open menu burger button' }).click();
-  await expect(page.getByRole('button', { name: 'Book consultation' })).toBeVisible();
-});
--- tests/test-7.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('about:blank');
-  await page.goto('chrome-error://chromewebdata/');
-  await page.goto('http://localhost:3000/');
-});
